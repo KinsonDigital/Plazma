641429815d7a5c569e19b3cdcffd5bd09550467d
Calvin Wilkinson <kinsondigital@gmail.com>
2024-06-08 14:07:09 +0100

    chore: add perf test project to test random service

---
+++ Performance/RandomPerf/Benchmarks.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="Benchmarks.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+// ReSharper disable ArrangeMethodOrOperatorBody
+namespace RandomPerf;
+
+using BenchmarkDotNet.Attributes;
+using Plazma.Services;
+
+/// <summary>
+/// Performance benchmarks for the <see cref="RandomizerService"/> class.
+/// </summary>
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private readonly RandomizerService randomService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Benchmarks"/> class.
+    /// </summary>
+    public Benchmarks() => this.randomService = new RandomizerService();
+
+    /// <summary>
+    /// Gets or sets the maximum range value.
+    /// </summary>
+    [Params(10, 100, 1_000, 10_000, 100_000)]
+    public int RangeMax { get; set; }
+
+    /// <summary>
+    /// Tests the <see cref="RandomizerService.GetValue(int,int)"/> overload of the <see cref="RandomizerService"/> class.
+    /// </summary>
+    [Benchmark]
+    public void GetValue_UsingIntParamOverload()
+    {
+        this.randomService.GetValue(0, RangeMax);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="RandomizerService.GetValue(float,float)"/> overload of the <see cref="RandomizerService"/> class.
+    /// </summary>
+    [Benchmark]
+    public void GetValue_UsingFloatParamOverload()
+    {
+        this.randomService.GetValue(0f, RangeMax);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="RandomizerService.GetValue(double,double)"/> overload of the <see cref="RandomizerService"/> class.
+    /// </summary>
+    [Benchmark]
+    public void GetValue_UsingDoubleParamOverload()
+    {
+        this.randomService.GetValue(0.0, RangeMax);
+    }
+}
---
+++ Performance/RandomPerf/Program.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace RandomPerf;
+
+using BenchmarkDotNet.Running;
+
+/// <summary>
+/// Main class for the application.
+/// </summary>
+internal static class Program
+{
+    /// <summary>
+    /// Main entry point for the application.
+    /// </summary>
+    /// <param name="args">Application arguments.</param>
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Benchmarks>();
+
+        Console.WriteLine(summary);
+        Console.ReadLine();
+    }
+}
