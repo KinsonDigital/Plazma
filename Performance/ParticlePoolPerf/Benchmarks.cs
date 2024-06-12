776466f80848ed2b5de0b09f9ba4bdd8c31399ee
Calvin Wilkinson <kinsondigital@gmail.com>
2024-06-12 10:36:18 +0100

    test: create perf project for particle pool

---
+++ Performance/ParticlePoolPerf/Benchmarks.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Benchmarks.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+namespace ParticlePoolPerf;
+
+using BenchmarkDotNet.Attributes;
+using PerfShared;
+using Plazma;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private ParticlePool<IFakeTexture>? pool;
+
+    /// <summary>
+    /// Gets or sets the total number of particles.
+    /// </summary>
+    [Params(100, 1_000, 10_000, 100_000, 1_000_000)]
+    public int TotalParticles { get; set; }
+
+    /// <summary>
+    /// Sets up the benchmark for the entire run.
+    /// </summary>
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var effect = new ParticleEffect
+        {
+            LimitSpawnRate = false,
+            TotalParticles = TotalParticles,
+        };
+        var fakeTextureLoader = new FakeTextureLoader();
+
+        this.pool = new ParticlePool<IFakeTexture>(effect, fakeTextureLoader);
+    }
+
+    /// <summary>
+    /// Runs the benchmark for the <see cref="ParticlePool{TTexture}.Update(TimeSpan)"/> method.
+    /// </summary>
+    // [Benchmark]
+    public void Update()
+    {
+        this.pool.Update(new TimeSpan(0, 0, 0, 0, 16));
+    }
+
+    /// <summary>
+    /// Runs the benchmark for the <see cref="ParticlePool{TTexture}.KillAllParticles"/> method.
+    /// </summary>
+    [Benchmark]
+    public void KillAllParticles()
+    {
+        this.pool.KillAllParticles();
+    }
+}
---
+++ Performance/ParticlePoolPerf/Program.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="Program.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using BenchmarkDotNet.Running;
+using ParticlePoolPerf;
+
+var summary = BenchmarkRunner.Run<Benchmarks>();
+
+Console.WriteLine(summary);
+Console.ReadLine();
